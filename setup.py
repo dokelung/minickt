78d50c5ce1c93e125022d03e1331af2b3760a0ca
dokelung <dokelung@gmail.com>
2014-10-15 15:41:38 +0800

    package already

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = 'minickt',
+	packages = ['minickt', 'cktParser', 'cktWriter'],
+	version = '0.6.5',
+	description = 'Analyzer and simulator of logic circuit',
+	author = 'dokelung',
+	author_email = 'dokelung@gmail.com',
+	url = 'https://github.com/dokelung/minickt',
+	download_url = 'https://github.com/dokelung/minickt/tarball/0.6.5',
+	keywords = ['circuit','eda','verification','logic','gate'],
+	classifiers = [],
+)
