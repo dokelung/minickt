d19d098c6cfa8d54dac5014868a8641481377d4c
dokelung <dokelung@gmail.com>
2014-10-16 09:52:19 +0800

    update setup.py by adding scripts

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
 	name = 'minickt',
 	packages = ['minickt', 'cktParser', 'cktWriter'],
+	scripts = ['minickt.py'],
 	version = '0.6.5',
 	description = 'Analyzer and simulator of logic circuit',
 	author = 'dokelung',
 	author_email = 'dokelung@gmail.com',
 	url = 'https://github.com/dokelung/minickt',
-	download_url = 'https://github.com/dokelung/minickt/tarball/0.6.5',
+	download_url = 'https://github.com/dokelung/minickt/tarball/v0.6.5',
 	keywords = ['circuit','eda','verification','logic','gate'],
 	classifiers = [],
 )
