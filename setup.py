31b51723173d57b4c7d29791a18307d7eb5457b1
dokelung <dokelung@gmail.com>
2014-10-23 15:43:15 +0800

    new version

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from distutils.core import setup
-
 setup(
-	name = 'minickt',
-	packages = ['minickt', 'cktParser', 'cktWriter'],
-	scripts = ['minic'],
-	install_requires=['matplotlib','networkx'],
-	version = '0.8',
-	description = 'Analyzer and simulator of logic circuit',
-	author = 'dokelung',
-	author_email = 'dokelung@gmail.com',
-	url = 'https://github.com/dokelung/minickt',
-	download_url = 'https://github.com/dokelung/minickt/tarball/v0.8',
-	keywords = ['circuit','eda','verification','logic','gate'],
-	classifiers = [],
+name = 'minickt',
+packages = ['minickt', 'cktParser', 'cktWriter'],
+scripts = ['minic'],
+install_requires=['matplotlib','networkx'],
+version = '0.8.1'
+description = 'Analyzer and simulator of logic circuit',
+author = 'dokelung',
+author_email = 'dokelung@gmail.com',
+url = 'https://github.com/dokelung/minickt',
+download_url = 'https://github.com/dokelung/minickt/tarball/v0.8.1'
+keywords = ['circuit','eda','verification','logic','gate'],
+classifiers = [],
 )
